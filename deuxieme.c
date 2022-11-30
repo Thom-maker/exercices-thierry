66fbd13e9fcc8fde0fee335e58d3ec6a361b0b89
Thom-Maaker <thomas.aumond@gmail.com>
2022-11-30 14:42:14 +0100

    modified:   deuxieme.c

--- deuxieme.c
+++ deuxieme.c
@@ -4,6 +4,34 @@
 
 int main(int argc, char *argv[])
 {
+    char alphabet[1];
+    char alphabet01[] = "a";
+    char alphabet02[] = "b";
+    char alphabet03[] = "c";  
+    char alphabet04[] = "d";
+    char alphabet05[] = "e";
+    char alphabet06[] = "f";
+    char alphabet07[] = "g";
+    char alphabet08[] = "h";
+    char alphabet09[] = "i";
+    char alphabet10[] = "j";
+    char alphabet11[] = "k";
+    char alphabet12[] = "l";
+    char alphabet13[] = "m";
+    char alphabet14[] = "n";
+    char alphabet15[] = "o";
+    char alphabet16[] = "p";
+    char alphabet17[] = "q";
+    char alphabet18[] = "r";
+    char alphabet19[] = "s";
+    char alphabet20[] = "t";
+    char alphabet21[] = "u";
+    char alphabet22[] = "v";
+    char alphabet23[] = "w";
+    char alphabet24[] = "x";
+    char alphabet25[] = "y";
+    char alphabet26[] = "2";
+    char lettre6[] = "n";
     int longueur_mot = 0;
     char mot[100] = "0";
     //char *cherche_mot;
@@ -22,5 +50,5 @@ int main(int argc, char *argv[])
         printf("Tu viens de taper la lettre %s !\n\n", lettre);
     }
         return 0;
-        
+
 }
